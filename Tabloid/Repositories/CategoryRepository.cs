d55c6fb281221ac2d2ae6ddf46527cd193382c84
Allison Carte <cartemack@gmail.com>
2022-11-16 15:25:04 -0500

    Back-end/C# Side of View

---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Category> GetAllCats()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name 
+                        FROM Category";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var cat = new List<Category>();
+                    while (reader.Read())
+                    {
+                        cat.Add(new Category()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name")
+
+                        });
+                    }
+                    reader.Close();
+                    return cat;
+                }
+            }
+        }
+
+
+    }
+}
