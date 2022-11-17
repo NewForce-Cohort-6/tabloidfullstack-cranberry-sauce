de4cf16d14e4159fc42da9a2aa835c81b2a7c869
Jeannette Geib <jeannettegeib@gmail.com>
2022-11-17 09:18:45 -0500

    first save

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
+import TagList from "./TagList";
 
 export default function ApplicationViews() {
 
  return(
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/TagManagement" element={<TagList />} />
       </Routes>
    );
  
