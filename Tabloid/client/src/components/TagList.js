86a028cd1d1b034c14bcae2e7a66af8d3e6b2799
Jeannette Geib <jeannettegeib@gmail.com>
2022-11-17 12:12:02 -0500

    Jeannette Create Tag Ticket 14

--- Tabloid/client/src/components/TagList.js
+++ Tabloid/client/src/components/TagList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import getAllTags from "../Managers/TagManager.js";
+import {getAllTags} from "../Managers/TagManager.js";
+import { AddTagForm } from "./AddTag.js";   
 
 const TagList = () =>{
     const [tags, setTags]=useState([]);
@@ -10,12 +11,15 @@ const TagList = () =>{
 
     useEffect(()=>{
         gettingTags();
-    },[])
+    },[tags])
     
     return(
+        <>
+        <AddTagForm />
         <div className="container" >
             {tags.map(t => <div className ="row"><div className = "column"> {t.name} </div></div>)}
         </div>
+        </>
     );
 
 };
